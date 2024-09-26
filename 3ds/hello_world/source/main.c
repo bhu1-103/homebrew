69bb40412b4e5e029c040dbfa6cdd4fe7d4e45ef
bhu1-103 <103.bhuvan@gmail.com>
2024-09-26 20:20:01 +0530

    more colors

--- 3ds/hello_world/source/main.c
+++ 3ds/hello_world/source/main.c
@@ -4,13 +4,13 @@
 int main(int argc, char **argv)
 {
 	#define RESET "\x1b[0m"
-	#define RED "\x1b[31m"
-	#define GREEN "\x1b[32m"
-	#define YELLOW "\x1b[33m"
-	#define BLUE "\x1b[34m"
-	#define MAGENTA "\x1b[35m"
-	#define CYAN "\x1b[36m"
-	#define WHITE "\x1b[37m"
+	#define RED "\x1b[31;1m"
+	#define GREEN "\x1b[32;1m"
+	#define YELLOW "\x1b[33;1m"
+	#define BLUE "\x1b[34;1m"
+	#define MAGENTA "\x1b[35;1m"
+	#define CYAN "\x1b[36;1m"
+	#define WHITE "\x1b[37;1m"
 	gfxInitDefault();
 
 	PrintConsole topScreen, bottomScreen;
