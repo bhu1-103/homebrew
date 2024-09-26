04ea0b669339bb970217e753f71d70dc99541a70
bhu1-103 <103.bhuvan@gmail.com>
2024-09-26 19:39:12 +0530

    rookie mistake

--- 3ds/hello_world/source/main.c
+++ 3ds/hello_world/source/main.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char **argv)
 {
-	#define RESET "\x1b[0m";
-	#define RED "\x1b[31m";
-	#define GREEN "\x1b[32m";
-	#define YELLOW "\x1b[33m";
-	#define BLUE "\x1b[34m";
-	#define MAGENTA "\x1b[35m";
-	#define CYAN "\x1b[36m";
-	#define WHITE "\x1b[37m";
+	#define RESET "\x1b[0m"
+	#define RED "\x1b[31m"
+	#define GREEN "\x1b[32m"
+	#define YELLOW "\x1b[33m"
+	#define BLUE "\x1b[34m"
+	#define MAGENTA "\x1b[35m"
+	#define CYAN "\x1b[36m"
+	#define WHITE "\x1b[37m"
 	gfxInitDefault();
 
 	PrintConsole topScreen, bottomScreen;
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	printf("hmmmmmmm\n");
 
 	consoleSelect(&bottomScreen);
-	printf(RED "even" GREEN "more" BLUE "hmmmmm");
+	printf(RED "even", GREEN "more", BLUE "hmmmmm");
 
 	consoleSelect(&topScreen);
 	printf("");
