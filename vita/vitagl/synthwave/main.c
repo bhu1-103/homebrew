d7e4874db1807cc048291623216ce4eb8b5262d5
bhu1-103 <103.bhuvan@gmail.com>
2024-12-20 23:58:04 +0530

    added standardized makefile

---
+++ vita/vitagl/synthwave/main.c
@@ -0,0 +1,33 @@
+#include <vitaGL.h>
+
+int main(){
+	
+	vglInit(0x800000);
+	
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 960, 544, 0, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	for (;;) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glBegin(GL_QUADS);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(400, 0, 0);
+		glColor3f(1.0, 1.0, 0.0);
+		glVertex3f(800, 0, 0);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(800, 400, 0);
+		glColor3f(1.0, 0.0, 1.0);
+		glVertex3f(400, 400, 0);
+		glEnd();
+		
+		vglSwapBuffers(GL_FALSE);
+	}
+	
+	vglEnd();
+}
